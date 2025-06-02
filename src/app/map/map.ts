1ab368c2f56ad5f95d980a96a5c4a0bb7328648c
ravinduramesh <ravinduramesh@gmail.com>
2025-06-02 18:37:34 +1200

    Fix routing machine marker icon issue.

--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -294,6 +294,11 @@ export class MapComponent implements AfterViewInit, OnDestroy {
       this.map.removeControl(this.routingControl);
     }
 
+    const startIcon =
+      'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%23660BC2"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z"/></svg>'; // Start marker (Purple)
+    const endIcon =
+      'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%231F75FE"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z"/></svg>'; // End marker (Blue)
+
     this.routingControl = L.Routing.control({
       waypoints: waypoints,
       routeWhileDragging: true, // Calculate route while dragging waypoints (if draggable)
@@ -305,28 +310,17 @@ export class MapComponent implements AfterViewInit, OnDestroy {
         extendToWaypoints: false,
         missingRouteTolerance: 0,
       },
-      // @ts-ignore: createMarker is a valid option for leaflet-routing-machine but not in its typings
+      createMarker: function (i: number, waypoint: any) {
+        return L.marker(waypoint.latLng, {
+          icon: L.icon({
+            iconUrl: i === 0 ? startIcon : endIcon,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+          }),
+          draggable: true,
+        });
+      },
     } as any).addTo(this.map);
-
-    // Set createMarker after control is created to avoid TypeScript error
-    (this.routingControl as any).options.createMarker = (
-      i: number,
-      waypoint: L.LatLng,
-      n: number
-    ) => {
-      // Custom markers for start (purple) and end (blue) of the route
-      const iconUrl =
-        i === 0
-          ? 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%23660BC2"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z"/></svg>' // Start marker (Purple)
-          : 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%231F75FE"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z"/></svg>'; // End marker (Blue)
-      return L.marker(waypoint, {
-        icon: L.icon({
-          iconUrl: iconUrl,
-          iconSize: [25, 41],
-          iconAnchor: [12, 41],
-        }),
-      });
-    };
 
     // Event listener for when routes are found
     this.routingControl.on('routesfound', (e: any) => {
