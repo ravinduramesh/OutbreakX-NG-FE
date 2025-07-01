c0989e1d8210a2636b6a625f461692b94636f6aa
ravinduramesh <ravinduramesh@gmail.com>
2025-07-01 23:48:10 +1200

    Add env files to setup firebase.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,65 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimations(),
+    provideFirebaseApp(() => {
+      const app = initializeApp(environment.firebase);
+      return app;
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+      return auth;
+    }),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      return firestore;
+    }),
+    importProvidersFrom(
+      MatSidenavModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatIconModule,
+      MatListModule,
+      MatDialogModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatCardModule,
+      MatSnackBarModule,
+      MatTooltipModule,
+      MatProgressSpinnerModule,
+      MatMenuModule,
+      MatDividerModule
+    ),
+  ],
 };
